cd4dada838adcd78d8a6a45deeb3a8206988f422
VladBullet <vladone1996@gmail.com>
2020-03-02 00:50:01 +0200

    QrCode Generation works + QrCodeScan verify with PI works

---
+++ DeltaQrCode/wwwroot/lib/Scripts/QrCodeCamera.js
@@ -0,0 +1,14 @@
+﻿
+function ShowHeaderAlert(response, type) {
+    var alertParam = $("#HeaderAlert");
+    console.log(response);
+    alertParam.text(response);
+    if (type == "success")
+        alertParam.addClass("alert-success");
+    else
+        alertParam.addClass("alert-danger");
+    alertParam.alert();
+    alertParam.fadeTo(2000, 500).slideUp(500, function () {
+        alertParam.slideUp(500);
+    });
+}
