cd58b8a147ebac8eba8bc7a0f7828a4dc64fbed0
VladBullet <vladone1996@gmail.com>
2020-03-01 14:27:36 +0200

    frontend add GenerateQrCode page. still needs work.

---
+++ DeltaQrCode/Controllers/QrCodeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeltaQrCode.Controllers
+{
+    public class QrCodeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //TODO: Add function to save the QrCode
+    }
+}
