4332edf6df2b19f6c5b60a927a721cda94e872aa
Vlad Bulete <vlad.bulete@proplanet.nl>
2021-04-02 14:48:07 +0300

    colors for status

--- DeltaQrCode/HelpersAndExtensions/ConstantsAndEnums.cs
+++ DeltaQrCode/HelpersAndExtensions/ConstantsAndEnums.cs
@@ -3,18 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DeltaQrCode.ModelsDto
+namespace DeltaQrCode
 {
-    using System.ComponentModel.DataAnnotations;
-    using System.Security.Policy;
-
     public static class ConstantsAndEnums
     {
 
         public static readonly Dictionary<int, string> RowsDictionary = new Dictionary<int, string> { { 1, "A" }, { 2, "B" }, { 3, "C" }, { 4, "D" }, { 5, "E" } };
 
         public static readonly string[] Intervals = { "A", "B", "C", "D", "E" };
-        
+
         public static readonly int RandMax = 20; // TODO : --- VLAD --- : MAKE SURE WHAT THE MAX LIMITS ARE AND SET THEM RIGHT
         public static readonly int PozMax = 3; // TODO : --- VLAD --- : MAKE SURE WHAT THE MAX LIMITS ARE AND SET THEM RIGHT
 
@@ -43,5 +40,12 @@ namespace DeltaQrCode.ModelsDto
         Interior = 1,
         Exterior = 2,
         InteriorExterior = 3
+    }
+    public enum StatusAnvelope
+    {
+        InRaft = 1,
+        Casate = 2,
+        Predate = 3,
+        Montate = 4
     }
 }
