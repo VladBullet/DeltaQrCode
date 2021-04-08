41baf707a86bd38d9fe62d5b6aaa357418d45df8
Fili <adrian.filipache0@gmail.com>
2021-04-08 13:41:52 +0300

    Fixed Diam

--- DeltaQrCode/HelpersAndExtensions/ConstantsAndEnums.cs
+++ DeltaQrCode/HelpersAndExtensions/ConstantsAndEnums.cs
@@ -12,7 +12,7 @@ namespace DeltaQrCode
 
         public static readonly Dictionary<int, string> TimeDictionary = new Dictionary<int, string> { { 30, "30 min" }, { 60, "1 ora" }, { 90, "1 ora 30 min" }, { 120, "2 ore" }, { 150, "2 ore 30 min" }, { 180, "3 ore" }, { 210, "3 ore 30 min" }, { 240, "4 ore" }, { 270, "4 ore 30 min" }, { 300, "5 ore" }, { 330, "5 ore 30 min" }, { 360, "6 ore" }, { 390, "6 ore 30 min" }, { 420, "7 ore" }, { 450, "7 ore 30 min" }, { 480, "8 ore" } };
 
-        public static readonly string[] Diametru = {"R13","R14", "R15", "R16", "R17", "R18", "R19", "R20", "R21", "R22"}; 
+        public static readonly Dictionary<int, string> DiametruDictionary = new Dictionary<int, string> { { 13, "R13" }, { 14, "R14" }, { 15, "R15" }, { 16, "R16" }, { 17, "R17" }, { 18, "R18" }, { 19, "R19" }, { 20, "R20" }, { 21, "R21" }, { 22, "R22" } };
 
         public static readonly string[] Latime = { "125", "135", "145", "155", "165", "175", "185", "195", "205", "215", "225", "235", "245", "255", "265", "275", "285", "295", "305", "315", "325", "335", "345", "355"}; 
 
