4731d63c8149a29437cc50f0d5a31871751d50b0
Fili <adrian.filipache0@gmail.com>
2021-04-20 16:46:01 +0300

    Operatiune schimb template

---
+++ DeltaQrCode/ViewModels/SchimbAnvelope/SchimbAnvelopeVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeltaQrCode.ViewModels.SchimbAnvelope
+{
+    public class SchimbAnvelopeVM
+    {
+        public int Id { get; set; }
+        public string NumarInmatriculare { get; set; }
+        public string PersoanaContact { get; set; }
+        public int? SetIesireId { get; set; }
+        public int SetIntrareId { get; set; }
+        public string Observatii { get; set; }
+        public DateTime DataSchimb { get; set; }
+        public int? UserId { get; set; }
+        public TimeSpan OraInceput { get; set; }
+        public TimeSpan? OraSfarsit { get; set; }
+        public bool OperatiuneFinalizata { get; set; }
+        public int PasCurentOperatiune { get; set; }
+    }
+}
